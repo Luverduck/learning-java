c0935386fbde6a2fe3ba9fdee16dadd8f2f7f7b0
Luverduck <hyekko92@gmail.com>
2024-07-07 06:18:41 +0900

    02.04. 자료형

---
+++ Section_02/src/s02_04/L04_01.java
@@ -0,0 +1,27 @@
+package s02_04;
+
+/**
+ * 자료형 (Data Type)
+ * 프로그래밍 언어에서 사용하는 데이터의 형태를 의미한다. (리터럴의 형태)
+ * 자료형에 따라 필요한 저장 공간의 크기와 저장 방식이 다르다.
+ */
+
+public class L04_01 {
+    public static void main(String[] args) {
+
+        // 자료형의 종류
+        // 1) 논리값 (참 / 거짓)
+        System.out.println(true);
+        System.out.println(false);
+
+        // 2) 숫자 (정수 / 실수)
+        System.out.println(100);
+        System.out.println(3.5);
+
+        // 3) 문자
+        System.out.println('c');
+
+        // 4) 문자열
+        System.out.println("Hello, World!");
+    }
+}
