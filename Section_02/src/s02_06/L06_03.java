1e76b24b80b835eed90c5e1e2b43892d276dc367
Luverduck <hyekko92@gmail.com>
2024-07-07 19:06:54 +0900

    02.06. 식별자와 예약어

---
+++ Section_02/src/s02_06/L06_03.java
@@ -0,0 +1,16 @@
+package s02_06;
+
+/**
+ * 예약어 (Keyword)
+ * 프로그래밍 언어에서 미리 사용을 예약한 단어
+ * Java의 예약어 목록 : https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+ */
+
+public class L06_03 {
+    public static void main(String[] args) {
+        // 예약어는 식별자로 사용할 수 없다.
+        //int double;          // double은 변수를 선언할 때 변수의 자료형을 나타내는 예약어이다.
+        //boolean instanceof;  // instanceof는 변수의 자료형을 반환하는 연산자로, 예약어이다.
+        //char new;            // new는 객체를 생성할 때 사용하는 연산자로, 예약어이다.
+    }
+}
