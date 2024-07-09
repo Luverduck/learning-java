ff4866d21af694a8b9929640e61006817bf8a892
Luverduck <hyekko92@gmail.com>
2024-07-09 23:17:50 +0900

    03.03. 자료형의 변환

--- Section_03/src/s03_03/L03_03.java
+++ Section_03/src/s03_03/L03_03.java
@@ -9,8 +9,7 @@ public class L03_03 {
     public static void main(String[] args) {
         // 자료형의 승격
         byte _1b_byte = 50;
-        _1b_byte = _1b_byte * 2;
-        // java: incompatible types: possible lossy conversion from int to byte
+        // _1b_byte = _1b_byte * 2;  // java: incompatible types: possible lossy conversion from int to byte
         // 1. 표현식 _1b_byte = _1b_byte * 2을 평가할 때 연산자 *에 대한 피연산자는 _1b_byte와 2이다.
         // 2. `byte`형 변수 `_1b_byte`와 `int`형 리터럴 `2`의 자료형이 다르므로 `_1b_byte`를 `int`로 승격시킨다.
         // 3. 자료형 승격 후 표현식 _1b_byte * 2을 평가한다. 이때 평가 결과의 자료형은 int이다.
