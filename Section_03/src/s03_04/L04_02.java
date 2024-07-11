995ddb71d481c259c9545611c200733238cfa09a
Luverduck <hyekko92@gmail.com>
2024-07-11 23:46:31 +0900

    03.04. 배열

--- Section_03/src/s03_04/L04_02.java
+++ Section_03/src/s03_04/L04_02.java
@@ -1,30 +1,35 @@
 package s03_04;
 
+import java.util.Arrays;
+
 /**
  * 배열의 초기화
- * 배열은 초기화 과정을 통해 배열 요소에 해당하는 메모리를 할당받는다.
- * 배열은 다음의 2가지 방식으로 초기화될 수 있다.
- * 1) new 키워드를 이용한 초기화
- * 2) 배열 리터럴을 이용한 초기화
+ * 배열 요소의 메모리를 할당한 후 해당 메모리의 시작 주소를 배열 변수에 저장하는 과정이다.
+ * 배열 변수의 메모리는 스택(Stack) 영역에, 배열 요소의 메모리는 힙(Heap) 영역에 할당된다.
  */
 
 public class L04_02 {
     public static void main(String[] args) {
-        // 'new' 키워드를 이용한 초기화
-        // 'new' 키워드는 메모리를 동적으로 할당한 후 해당 메모리의 주소를 반환하는 연산자(opeartor)이다.
-        // 'new' 키워드를 통해 배열 변수를 초기화할 수 있다.
+        // ✅ 배열 변수는 new 키워드를 이용하여 초기화할 수 있다.
+        // new 키워드는 메모리를 동적 할당 후 해당 메모리의 시작 주소를 반환하는 연산자(opeartor)이다.
         short[]     arrShort    = new short[3];
-
-        // 'new' 키워드로 배열 변수를 초기화하는 과정은 다음과 같다.
-        // 1. 배열 요소가 될 자료형 크기 메모리를 `[]` 안의 수만큼 동적으로 할당한다.
-        // 2. 동적으로 할당한 메모리를 기본값으로 초기화한다.
-        // 3. 초기화가 끝나면 해당 메모리의 시작 주소를 배열 변수에 저장한다.
-
-        // 배열 변수를 초기화할 때 배열의 모든 요소는 기본값으로 초기화된다.
+        // new 키워드로 배열 변수를 초기화할 때 배열의 모든 요소는 기본값으로 초기화된다.
         int[]       arrInt      = new int[3];       // { 0, 0, 0 }
         double[]    arrDouble   = new double[3];    // { 0.0, 0.0, 0.0 }
         boolean[]   arrBoolean  = new boolean[3];   // { false, false, false }
         char[]      arrChar     = new char[3];      // { '\u0000', '\u0000', '\u0000' }
         String[]    arrString   = new String[3];    // { null, null, null }
+
+        // ✅ 배열 변수를 초기화할 때 배열 초기화자(Array Initializer)로 배열 요소들을 초기화할 수 있다.
+        byte[]      arrByte     = new byte[] { 1, 2, 3 };
+
+        // ✅ 배열 변수를 초기화할 때 new 키워드 없이 배열 초기화자 만으로 배열 요소들을 초기화할 수 있다.
+        long[]      arrLong     = { 1L, 2L, 3L };
+        // * 해당 방식의 초기화는 배열을 선언할 때 최초 1회만 사용할 수 있다.
+        // arrLong = { 4L, 5L, 6L }; // Array initializer is not allowed here
+
+        // ✅ 배열의 길이는 배열의 length 속성에 정의되어 있다.
+        int[] arr = new int[] {1, 2, 3, 4, 5};
+        int arrLength = arr.length; // 5
     }
 }
