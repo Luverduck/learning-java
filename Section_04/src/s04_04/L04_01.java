dfabf910149c681517bc6f09ffedd0f67b22c797
Luverduck <hyekko92@gmail.com>
2024-07-13 23:27:13 +0900

    04-04. 산술 연산자

---
+++ Section_04/src/s04_04/L04_01.java
@@ -0,0 +1,21 @@
+package s04_04;
+
+/**
+ * 산술 연산자(Arithmetic Operator)
+ * 피연산자간의 산술 연산을 수행하는 연산자
+ */
+
+public class L04_01 {
+    public static void main(String[] args) {
+        // +	왼쪽 피연산자에 오른쪽 피연산자를 더한다.
+        System.out.println(10 + 2);
+        // -	왼쪽 피연산자에 오른쪽 피연산자를 뺀다.
+        System.out.println(10 - 2);
+        // *	왼쪽 피연산자에 오른쪽 피연산자를 곱한다.
+        System.out.println(10 * 2);
+        // /	왼쪽 피연산자에 오른쪽 피연산자를 나눈 몫을 구한다.
+        System.out.println(10 / 2);
+        // %	왼쪽 피연산자에 오른쪽 피연산자를 나눈 나머지를 구한다.
+        System.out.println(10 % 2);
+    }
+}
