7727f9ff7ee34cf1d4a66ee82dce9818c8707e9b
Luverduck <hyekko92@gmail.com>
2024-07-13 21:56:20 +0900

    03.06. 자료형 추론 (Java 10+)

--- Section_03/src/s03_06/L06_02.java
+++ Section_03/src/s03_06/L06_02.java
@@ -11,14 +11,14 @@ public class L06_02 {
         // 자료형 추론의 특징
         // 1. 다음의 경우 자료형 추론을 사용할 수 없다.
         // i) 변수를 선언만 하는 경우
-        var varShort;
+        //var varShort;
         // ii) 변수를 null로 초기화하는 경우
-        var varString = null;
+        //var varString = null;
 
         // 2. 자료형 추론을 통해 초기화된 변수에 다른 자료형의 리터럴을 대입할 수 없다.
         // 리터럴 1은 정수형이므로 var는 int형으로 추론된다.
         var varInt = 1;
         // int형으로 추론된 varInt에 double형 리터럴 3.5는 대입할 수 없다.
-        varInt = 3.5;
+        //varInt = 3.5;
     }
 }
