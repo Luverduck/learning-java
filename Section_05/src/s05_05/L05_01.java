fe7d91a9e272dca99f5d0389d5cad69277a985c6
Luverduck <hyekko92@gmail.com>
2024-07-14 20:02:51 +0900

    05.05. for 문

---
+++ Section_05/src/s05_05/L05_01.java
@@ -0,0 +1,18 @@
+package s05_05;
+
+/**
+ * for 문
+ * for 문의 조건식 결과가 true인 동안 for 문의 코드 블록 안에 있는 문장을 반복 실행한다.
+ *
+ * for 문의 실행 과정
+ * 1. for 문의 초기식 실행
+ * 2. for 문의 조건식 결과가 true인 경우 for 문의 코드 블록 안에 있는 문장 실행
+ * 3. for 문의 증감식 실행
+ * 4. 이후 2와 3의 과정을 반복
+ */
+
+public class L05_01 {
+    public static void main(String[] args) {
+
+    }
+}
