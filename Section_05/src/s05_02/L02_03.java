2a5a10a421a97d611395d53e10abf39a70471929
Luverduck <hyekko92@gmail.com>
2024-07-14 16:25:29 +0900

    05.02. if 문

--- Section_05/src/s05_02/L02_03.java
+++ Section_05/src/s05_02/L02_03.java
@@ -5,6 +5,7 @@ package s05_02;
  * if 문의 조건식 결과가 true이면 if 문의 코드 블록 안에 있는 문장을 실행한다.
  * 그렇지 않으면 else if 문의 조건식 결과가 true이면 else if 문의 코드 블록 안에 있는 문장을 실행한다.
  * if 문과 모든 else if 문의 조건식 결과가 false이면 else 문의 코드 블록 안에 있는 문장을 실행한다.
+ * ⭐ if 문 / else if 문 / else 문의 코드 블록 안에 있는 문장 실행에 대한 선택 (다중 분기)
  */
 
 public class L02_03 {
