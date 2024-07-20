3a9016aeef15afa0332e9c43af7364d519d989b5
Luverduck <hyekko92@gmail.com>
2024-07-21 05:58:03 +0900

    07.03. 생성자

---
+++ Section_07/src/s07_03/L03_02/Main.java
@@ -0,0 +1,17 @@
+package s07_03.L03_02;
+
+/**
+ * 클래스의 인스턴스화 (instantiation)
+ * 객체의 생성은 `new` 키워드와 클래스의 *성자 호출을 통해 이루어진다.
+ * 클래스로부터 객체를 생성하는 과정을 클래스의 인스턴스화(instantiation)라고 하며
+ * 클래스의 인스턴스화로 생성된 객체를 클래스의 인스턴스(instance)라 한다.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // 생성자 호출을 통한 객체 생성
+        Person person1 = new Person();
+        Person person2 = new Person("Sam");
+        Person person3 = new Person("Sam", 25);
+    }
+}
