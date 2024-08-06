ff77c28a5cdae3cb2e81befe87f3f9b5222b9724
Luverduck <hyekko92@gmail.com>
2024-08-06 21:51:05 +0900

    09.01. 인터페이스

---
+++ Section_09/src/s09_01/L01_02/MyClass.java
@@ -0,0 +1,21 @@
+package s09_01.L01_02;
+
+/**
+ * 인터페이스의 구현
+ * implements 키워드를 통해 인터페이스를 클래스에 상속할 수 있다.
+ * 인터페이스를 클래스에 상속하는 과정을 인터페이스의 구현(implementation)이라 한다.
+ * 인터페이스를 상속받은 클래스는 반드시 인터페이스의 추상 메소드를 구현해야 한다.
+ */
+
+public class MyClass implements MyInterface {
+
+    // 필드 (Field)
+    String classField = "클래스의 필드";
+
+    // 메소드 (Method)
+    @Override
+    public void print() {
+        System.out.println(interfaceField); // 인터페이스의 final 필드에 접근 가능
+        System.out.println(classField);
+    }
+}
---
+++ Section_09/src/s09_01/L01_02/MyInterface.java
@@ -0,0 +1,11 @@
+package s09_01.L01_02;
+
+public interface MyInterface {
+
+    // ✅ final 필드
+    final String interfaceField = "인터페이스의 final 필드";
+
+    // ✅ 추상 메소드
+    void print();
+
+}
