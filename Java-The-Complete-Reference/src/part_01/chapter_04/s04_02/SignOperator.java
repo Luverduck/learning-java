49184d807d54e08a4660015c418d66e1c31e8838
Luverduck <hyekko92@gmail.com>
2024-09-23 02:39:32 +0900

    04.02. 부호 연산자

---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_02/SignOperator.java
@@ -0,0 +1,12 @@
+package part_01.chapter_04.s04_02;
+
+public class SignOperator {
+    public static void main(String[] args) {
+
+        // 부호 연산자 (Sign Operator)
+        int num = 10;
+        System.out.println(+num);
+        System.out.println(-num);
+
+    }
+}
