4ca133fbf7fc485f61fa91f2a22629b3d045646e
Luverduck <hyekko92@gmail.com>
2024-09-23 20:57:02 +0900

    04.08. 조건 연산자

---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_08/t08_01/ConditionalOperator.java
@@ -0,0 +1,12 @@
+package part_01.chapter_04.s04_08.t08_01;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+
+        // 조건 연산자 (Conditional Operator)
+        int x = 5;
+        int y = 3;
+        System.out.println( (x > y ? x + y : x - y) );
+
+    }
+}
