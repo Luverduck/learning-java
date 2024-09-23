f52cef2dcbf977458ac373c651903daab5def88b
Luverduck <hyekko92@gmail.com>
2024-09-23 20:56:45 +0900

    04.06. 비교 연산자

---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_06/t06_01/RelationalOperator.java
@@ -0,0 +1,16 @@
+package part_01.chapter_04.s04_06.t06_01;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+
+        // 비교 연산자 (Relational Operator)
+        int x = 1;
+        int y = 2;
+        System.out.println( "x == y : " + (x == y) );
+        System.out.println( "x != y : " + (x != y) );
+        System.out.println( "x > y : " + (x > y) );
+        System.out.println( "x >= y : " + (x >= y) );
+        System.out.println( "x < y : " + (x < y) );
+        System.out.println( "x <= y : " + (x <= y) );
+    }
+}
