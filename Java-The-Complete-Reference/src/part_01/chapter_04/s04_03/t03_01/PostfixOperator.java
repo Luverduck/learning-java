6f9698427f7bdc11ec46559b9880203188b0ccd9
Luverduck <hyekko92@gmail.com>
2024-09-23 20:56:13 +0900

    04.03. 증감 연산자

---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_03/t03_01/PostfixOperator.java
@@ -0,0 +1,14 @@
+package part_01.chapter_04.s04_03.t03_01;
+
+public class PostfixOperator {
+    public static void main(String[] args) {
+
+        // 후위 증감 연산자 (Postfix Increment and Decrement Operator)
+        int x = 0;
+        // 출력이 이루어진 후 값을 증가
+        System.out.println(x++);    // 0
+        System.out.println(x++);    // 1
+        System.out.println(x++);    // 2
+
+    }
+}
---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_03/t03_01/PrefixOperator.java
@@ -0,0 +1,14 @@
+package part_01.chapter_04.s04_03.t03_01;
+
+public class PrefixOperator {
+    public static void main(String[] args) {
+
+        // 전위 증감 연산자 (Prefix Increment and Decrement Operator)
+        int x = 0;
+        // 값을 증가시킨 후 출력
+        System.out.println(++x);    // 1
+        System.out.println(++x);    // 2
+        System.out.println(++x);    // 3
+
+    }
+}
