f0976b8697a5b2423e2d5083e8dfff0559bbb870
Luverduck <hyekko92@gmail.com>
2024-09-23 20:56:22 +0900

    04.04. 산술 연산자

---
+++ Java-The-Complete-Reference/src/part_01/chapter_04/s04_04/t04_01/ArithmeticOperator.java
@@ -0,0 +1,16 @@
+package part_01.chapter_04.s04_04.t04_01;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+        // 산술 연산자(Arithmetic Operator)
+        int x = 7;
+        int y = 4;
+        System.out.println( "x + y : " + (x + y) );
+        System.out.println( "x - y : " + (x - y) );
+        System.out.println( "x * y : " + (x * y) );
+        System.out.println( "x / y : " + (x / y) );
+        System.out.println( "x %% y : " + (x % y) );
+
+    }
+}
