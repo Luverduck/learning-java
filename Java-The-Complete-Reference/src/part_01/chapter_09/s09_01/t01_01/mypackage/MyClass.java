ae8d58115ef727244a51fccfece198d98986614e
Luverduck <hyekko92@gmail.com>
2024-10-09 20:46:39 +0900

    09.01. 패키지

---
+++ Java-The-Complete-Reference/src/part_01/chapter_09/s09_01/t01_01/mypackage/MyClass.java
@@ -0,0 +1,5 @@
+package part_01.chapter_09.s09_01.t01_01.mypackage;
+
+public class MyClass {
+
+}
---
+++ Java-The-Complete-Reference/src/part_01/chapter_09/s09_01/t01_01/mypackage/MyRecord.java
@@ -0,0 +1,4 @@
+package part_01.chapter_09.s09_01.t01_01.mypackage;
+
+public record MyRecord() {
+}
