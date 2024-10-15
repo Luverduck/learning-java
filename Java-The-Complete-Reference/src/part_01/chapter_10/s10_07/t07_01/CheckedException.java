d5f898cdc89f5f623c0639e929445097115d9cbb
Luverduck <hyekko92@gmail.com>
2024-10-15 23:56:31 +0900

    10.07. 표준 예외 클래스

---
+++ Java-The-Complete-Reference/src/part_01/chapter_10/s10_07/t07_01/CheckedException.java
@@ -0,0 +1,17 @@
+package part_01.chapter_10.s10_07.t07_01;
+
+public class CheckedException {
+    public static void main(String[] args) {
+
+        // 검사 예외 클래스 (Checked Exception)
+        ClassNotFoundException          ce1;
+        CloneNotSupportedException      ce2;
+        IllegalAccessException          ce3;
+        InstantiationException          ce4;
+        InterruptedException            ce5;
+        NoSuchFieldException            ce6;
+        NoSuchMethodException           ce7;
+        ReflectiveOperationException    ce8;
+
+    }
+}
---
+++ Java-The-Complete-Reference/src/part_01/chapter_10/s10_07/t07_02/UncheckedException.java
@@ -0,0 +1,28 @@
+package part_01.chapter_10.s10_07.t07_02;
+
+public class UncheckedException {
+    public static void main(String[] args) {
+
+        // 비검사 예외 클래스 (Unchecked Exception)
+        ArithmeticException                 uce1;
+        ArrayIndexOutOfBoundsException      uce2;
+        ArrayStoreException                 uce3;
+        ClassCastException                  uce4;
+        EnumConstantNotPresentException     uce5;
+        IllegalArgumentException            uce6;
+        IllegalCallerException              uce7;
+        IllegalMonitorStateException        uce8;
+        IllegalStateException               uce9;
+        IllegalThreadStateException         uce10;
+        IndexOutOfBoundsException           uce11;
+        LayerInstantiationException         uce12;
+        NegativeArraySizeException          uce13;
+        NullPointerException                uce14;
+        NumberFormatException               uce15;
+        SecurityException                   uce16;
+        StringIndexOutOfBoundsException     uce17;
+        TypeNotPresentException             uce18;
+        UnsupportedOperationException       uce19;
+
+    }
+}
