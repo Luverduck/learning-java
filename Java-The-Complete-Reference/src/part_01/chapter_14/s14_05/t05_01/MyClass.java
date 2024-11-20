c3fdc13e43f77dbaeecdd690cc178f79469db410
Luverduck <hyekko92@gmail.com>
2024-11-20 20:36:58 +0900

    14.05. 제네릭 메소드

---
+++ Java-The-Complete-Reference/src/part_01/chapter_14/s14_05/t05_01/MyClass.java
@@ -0,0 +1,16 @@
+package part_01.chapter_14.s14_05.t05_01;
+
+public class MyClass {
+
+    // 제네릭 메소드 (Generic Method)
+    public <T> int methodGeneric(T t) {
+        // ...
+        return 0;
+    };
+
+    public <T, U, V> int methodGeneric(T t, U u, V v) {
+        // ...
+        return 0;
+    };
+
+}
---
+++ Java-The-Complete-Reference/src/part_01/chapter_14/s14_05/t05_02/MyClass.java
@@ -0,0 +1,12 @@
+package part_01.chapter_14.s14_05.t05_02;
+
+public class MyClass {
+
+    // 제네릭 생성자 (Generic Constructor)
+    <T> MyClass(T t) {
+
+        // ...
+
+    }
+
+}
