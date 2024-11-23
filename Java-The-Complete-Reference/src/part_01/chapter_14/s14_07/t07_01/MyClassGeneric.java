7a731c0568fdf4755b1b93cdceeb81b288c81873
Luverduck <hyekko92@gmail.com>
2024-11-23 20:43:51 +0900

    14.07. 제네릭 타입의 계층 구조

---
+++ Java-The-Complete-Reference/src/part_01/chapter_14/s14_07/t07_01/MyClassGeneric.java
@@ -0,0 +1,7 @@
+package part_01.chapter_14.s14_07.t07_01;
+
+public class MyClassGeneric<T> {
+
+    // ...
+
+}
---
+++ Java-The-Complete-Reference/src/part_01/chapter_14/s14_07/t07_02/MyInterfaceGenericSub.java
@@ -0,0 +1,7 @@
+package part_01.chapter_14.s14_07.t07_02;
+
+public interface MyInterfaceGenericSub<T extends Number> extends MyInterfaceGeneric<T> {
+
+    // ...
+
+}
