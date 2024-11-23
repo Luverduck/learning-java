fbb33c45ae558e18d3fd7f5208e3027f7b028b36
Luverduck <hyekko92@gmail.com>
2024-11-23 19:16:25 +0900

    14.06. 제네릭 인터페이스

--- Java-The-Complete-Reference/src/part_01/chapter_14/s14_06/t06_03/MyInterfaceImplGeneric.java
+++ Java-The-Complete-Reference/src/part_01/chapter_14/s14_06/t06_03/MyInterfaceImplGeneric.java
@@ -1,6 +1,6 @@
 package part_01.chapter_14.s14_06.t06_03;
 
-public class MyInterfaceImplGeneric<U extends Number> implements MyInterfaceGeneric<U> {
+public class MyInterfaceImplGeneric<T extends Number> implements MyInterfaceGeneric<T> {
 
     // ...
 
